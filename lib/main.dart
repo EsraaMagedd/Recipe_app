dac5c7d99efa51f0c8a0392148b7d67c17e2f978
habiba333 <habiba.mo.ali3@gmail.com>
2023-09-05 11:03:34 +0300

    add Splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:home_screen/profile.dart';
+import 'splash.dart';
 
 import 'home.dart';
 import 'onboarding.dart';
@@ -28,7 +29,7 @@ class _MyAppState extends State<MyApp> {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home:  MyHomePage(),
+      home:  SplashScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
---
+++ lib/splash.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  Color _color = Color(0xfff94a0c);
+  Text _text1 = Text("Yummy",style: GoogleFonts.lobster(textStyle: TextStyle(color: Colors.white),fontSize: 70));
+  @override
+  void initState() {
+    Future.delayed(Duration(seconds: 2)).then((value) {
+      _color = Colors.white;
+      _text1 = Text("Yummy",style: GoogleFonts.lobster(textStyle: TextStyle(color: Color(0xfff94a0c)),fontSize: 70));
+      setState(() {});
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: _color,
+        body: Center(
+          child: Container(
+            height: 250,
+            width: 250,
+            child:_text1,
+          ),
+        )
+    );
+  }
+}
