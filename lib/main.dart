742b36a3d91d7352314c835a852e205b2df344a9
habiba333 <habiba.mo.ali3@gmail.com>
2023-09-05 11:20:50 +0300

    add Recipe Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:home_screen/profile.dart';
+import 'RecipeScreen.dart';
 import 'splash.dart';
 
 import 'home.dart';
@@ -29,7 +30,7 @@ class _MyAppState extends State<MyApp> {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home:  SplashScreen(),
+      home:  RecipeScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
